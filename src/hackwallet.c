7530ce6db2c5fcae6f0ba32e0f79410f1095fcf0
Charles Lehner <celehner1@gmail.com>
2014-07-12 18:26:39 -0400

    Add send dialog

--- src/hackwallet.c
+++ src/hackwallet.c
@@ -1,10 +1,10 @@
 #include <stdlib.h>
-#include <signal.h>
 #include <gtk/gtk.h>
 #include <glib/gi18n.h>
 
 // Resources
-//#define PREFERENCES_UI_FILE   DATADIR "/gui/preferences.ui"
+#define DATADIR "data"
+#define SEND_UI_FILE DATADIR "/gui/send.glade"
 #define STATUS_ICON "/usr/share/icons/Numix/16x16/actions/stock_new-meeting.png"
 
 // About
@@ -14,45 +14,118 @@
 #define COMMENTS "Bitcoin wallet for your system tray."
 #define WEBSITE "http://github.com/clehner/hackwallet"
 
+// Type definitions
+
 struct menuThing {
     const char *mnemonic;
     void (*handler) (GtkMenuItem *menuItem, gpointer userData);
 };
 
+typedef struct
+{
+    GtkWidget * dialog;
+    GtkEntry * address_entry;
+    GtkEntry * amount_entry;
+    GtkEntry * description_entry;
+    GtkEntry * fee_entry;
+    GtkButton * cancel_button;
+    GtkButton * send_button;
+} SendGui;
+
 GtkStatusIcon *statusIcon;
 GtkWidget *appMenu;
 GtkWidget *settingsMenu;
+SendGui *sendGui = NULL;
+
+// Send dialog handlers
+//
+static gboolean send_dialog_delete_event(GtkWidget * widget,
+    GdkEvent * event, gpointer user_data)
+{
+    gtk_widget_destroy(sendGui->dialog);
+    return FALSE;
+}
+
+static void send_dialog_destroy(GObject * object, gpointer user_data)
+{
+    g_free(sendGui);
+    sendGui = NULL;
+}
+
+static void send_cancel_button_clicked(GtkWidget * widget,
+    gpointer user_data)
+{
+    gtk_widget_destroy(sendGui->dialog);
+}
+
+// Status menu handlers
 
 static void status_icon_on_popup_menu(GtkStatusIcon * status_icon, guint button,
         guint activation_time, gpointer user_data)
 {
     // Show the settings menu
-	gtk_widget_show_all(settingsMenu);
+    gtk_widget_show_all(settingsMenu);
 
-	gtk_menu_popup(GTK_MENU(settingsMenu), NULL, NULL,
-		gtk_status_icon_position_menu, status_icon,
-		button, activation_time);
+    gtk_menu_popup(GTK_MENU(settingsMenu), NULL, NULL,
+        gtk_status_icon_position_menu, status_icon,
+        button, activation_time);
 }
 
 static gboolean status_icon_on_button_press(GtkStatusIcon * status_icon,
-	GdkEventButton * event, gpointer user_data)
+    GdkEventButton * event, gpointer user_data)
 {
-	if (event->button != 1) {
+    // Show the app menu on left click
+    if (event->button != 1) {
         return FALSE;
     }
 
-	gtk_widget_show_all(appMenu);
+    gtk_widget_show_all(appMenu);
 
-	gtk_menu_popup(GTK_MENU(appMenu), NULL, NULL,
-		gtk_status_icon_position_menu, status_icon,
-		event->button, event->time);
+    gtk_menu_popup(GTK_MENU(appMenu), NULL, NULL,
+        gtk_status_icon_position_menu, status_icon,
+        event->button, event->time);
 
     return TRUE;
 }
 
 static void menu_on_send(GtkMenuItem * menuItem, gpointer userData)
 {
-    printf("send\n");
+    // Create window with send form
+    if (sendGui) {
+        gtk_window_present(GTK_WINDOW(sendGui->dialog));
+        return;
+    }
+
+    sendGui = (SendGui *)g_malloc(sizeof *sendGui);
+
+    GtkBuilder *builder = gtk_builder_new();
+    gtk_builder_add_from_file(builder, SEND_UI_FILE, NULL);
+
+    // Get widgets from builder
+    #define getobj(x) gtk_builder_get_object(builder, x)
+    sendGui->dialog = GTK_WIDGET(getobj("dialog"));
+    sendGui->address_entry = GTK_ENTRY(getobj("address"));
+    sendGui->amount_entry = GTK_ENTRY(getobj("amount"));
+    sendGui->description_entry = GTK_ENTRY(getobj("description"));
+    sendGui->fee_entry = GTK_ENTRY(getobj("fee"));
+    sendGui->cancel_button = GTK_BUTTON(getobj("close"));
+    sendGui->send_button = GTK_BUTTON(getobj("send"));
+    #undef getobj
+
+    g_object_unref(G_OBJECT(builder));
+
+    //gtk_window_set_default_icon_from_file(APP_ICON, NULL);
+
+    // Connect signals
+    g_signal_connect(G_OBJECT(sendGui->dialog), "destroy", G_CALLBACK(
+        send_dialog_destroy), NULL);
+    g_signal_connect(G_OBJECT(sendGui->dialog), "delete-event", G_CALLBACK(
+        send_dialog_delete_event), NULL);
+    g_signal_connect(G_OBJECT(sendGui->cancel_button), "clicked", G_CALLBACK(
+        send_cancel_button_clicked), NULL);
+
+    //gtk_window_set_modal(GTK_WINDOW(sendGui->dialog), TRUE);
+    gtk_widget_show_all(sendGui->dialog);
 }
 
 static void menu_on_receive(GtkMenuItem * menuItem, gpointer userData)
@@ -67,16 +140,16 @@ static void menu_on_quit(GtkMenuItem * menuItem, gpointer userData)
 
 static void menu_on_about(GtkMenuItem * menuItem, gpointer userData)
 {
-	GtkWidget * aboutDialog = gtk_about_dialog_new();
-	gtk_about_dialog_set_program_name(GTK_ABOUT_DIALOG(aboutDialog), APPNAME);
-	gtk_about_dialog_set_version(GTK_ABOUT_DIALOG(aboutDialog), VERSION);
-	gtk_about_dialog_set_logo(GTK_ABOUT_DIALOG(aboutDialog),
-		gdk_pixbuf_new_from_file(STATUS_ICON, NULL));
-	gtk_about_dialog_set_copyright(GTK_ABOUT_DIALOG(aboutDialog), COPYRIGHT);
-	gtk_about_dialog_set_comments(GTK_ABOUT_DIALOG(aboutDialog), COMMENTS);
-	gtk_about_dialog_set_website(GTK_ABOUT_DIALOG(aboutDialog),	WEBSITE);
-	gtk_dialog_run(GTK_DIALOG(aboutDialog));
-	gtk_widget_destroy(aboutDialog);
+    GtkWidget * aboutDialog = gtk_about_dialog_new();
+    gtk_about_dialog_set_program_name(GTK_ABOUT_DIALOG(aboutDialog), APPNAME);
+    gtk_about_dialog_set_version(GTK_ABOUT_DIALOG(aboutDialog), VERSION);
+    gtk_about_dialog_set_logo(GTK_ABOUT_DIALOG(aboutDialog),
+        gdk_pixbuf_new_from_file(STATUS_ICON, NULL));
+    gtk_about_dialog_set_copyright(GTK_ABOUT_DIALOG(aboutDialog), COPYRIGHT);
+    gtk_about_dialog_set_comments(GTK_ABOUT_DIALOG(aboutDialog), COMMENTS);
+    gtk_about_dialog_set_website(GTK_ABOUT_DIALOG(aboutDialog), WEBSITE);
+    gtk_dialog_run(GTK_DIALOG(aboutDialog));
+    gtk_widget_destroy(aboutDialog);
 }
 
 #define SEPARATOR {(char *)_separator, NULL}
@@ -122,27 +195,27 @@ GtkWidget *buildMenu(struct menuThing *spec) {
 }
 
 int main(int argc, char *argv[]) {
-	// Initialize gtk with arguments
-	GError * error = 0;
-	GOptionEntry options[] = {
-		{ NULL }
-	};
-	if(!gtk_init_with_args(&argc, &argv, "", options, "", &error)) {
-		if(error) {
-			g_printerr("%s\n", error->message);
-		}
-		return EXIT_FAILURE;
-	}
-	signal(SIGCHLD, SIG_IGN);
+    // Initialize gtk with arguments
+    GError * error = 0;
+    GOptionEntry options[] = {
+        { NULL }
+    };
+    if(!gtk_init_with_args(&argc, &argv, "", options, "", &error)) {
+        if(error) {
+            g_printerr("%s\n", error->message);
+        }
+        return EXIT_FAILURE;
+    }
+    signal(SIGCHLD, SIG_IGN);
 
     // Set up status icon
     statusIcon = gtk_status_icon_new_from_file(STATUS_ICON);
-	gtk_status_icon_set_visible(statusIcon, TRUE);
+    gtk_status_icon_set_visible(statusIcon, TRUE);
 
-	g_signal_connect(G_OBJECT(statusIcon), "popup-menu",
-		G_CALLBACK(status_icon_on_popup_menu), NULL);
-	g_signal_connect(G_OBJECT(statusIcon), "button_press_event",
-		G_CALLBACK(status_icon_on_button_press), NULL);
+    g_signal_connect(G_OBJECT(statusIcon), "popup-menu",
+        G_CALLBACK(status_icon_on_popup_menu), NULL);
+    g_signal_connect(G_OBJECT(statusIcon), "button_press_event",
+        G_CALLBACK(status_icon_on_button_press), NULL);
 
     // Set up menus
     appMenu = buildMenu(appMenuSpec);
